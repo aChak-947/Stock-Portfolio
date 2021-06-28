aaa5e1f75104fb0e54c086f2626b0bb24964ad7d
Avronil <avronilchakraborty947@gmail.com>
2021-06-28 12:02:33 +0530

    initial commit

---
+++ quotes/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+	import requests
+	import json
+
+	if request.method == 'POST':
+		ticker = request.POST['ticker']
+		api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_6e72bad9cf364758af8fd4176650482d")
+
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+
+		return render(request, 'home.html', {'api': api})
+
+	else:
+		return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above"})
+
+	
+
+
+
+def about(request):
+	return render(request, 'about.html', {})
+
+
+
+
+def add_stock(request):
+	return render(request, 'add_stock.html', {})
